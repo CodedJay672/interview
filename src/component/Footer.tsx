db457919bcd0371d1342e4000f2ea03bcc4fc756
codedjay672 <emmajoel6456@gmail.com>
2025-05-05 21:00:10 +0100

    fix: correct typo in footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="w-full flex items-center justify-center p-6">
       <span className="text-accent font-light text-sm">
-        copywrite &copy; {new Date().getFullYear()}
+        copyright &copy; {new Date().getFullYear()}
       </span>
     </footer>
   );
